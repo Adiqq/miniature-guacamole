adb25defc837e51924c4708acfa866802ce2904a
Adrian Wawrzak <Adiqq@users.noreply.github.com>
2017-10-23 22:31:20 +0200

    content

--- scripts.js
+++ scripts.js
@@ -6,23 +6,23 @@ function addClickHandler(query, handler) {
 }
 
 window.onload = function () {
-    addClickHandler('.menu .home', function () {
-        changeActive('home');
+    addClickHandler('nav ul li.blog', function () {
+        changeActive('blog');
     });
-    addClickHandler('.menu .about', function (){
+    addClickHandler('nav ul li.about', function (){
         changeActive('about');
     });
-    addClickHandler('.menu .contact', function (){
+    addClickHandler('nav ul li.contact', function (){
         changeActive('contact');
     });
-    addClickHandler('.menu .references', function (){
-        changeActive('references');
+    addClickHandler('nav ul li.skills', function (){
+        changeActive('skills');
     });
-    changeActive('home');
+    changeActive('blog');
 }
 
 function showActivePage(name){
-    var sections = document.querySelectorAll('main section');
+    var sections = document.querySelectorAll('section');
     for(var i = 0; i < sections.length; i++){
         if(sections[i].className === name){
             sections[i].style.display = 'block';
@@ -33,9 +33,22 @@ function showActivePage(name){
 
 }
 
+function setActiveMenuItem(name){
+    var menuItems = document.querySelectorAll('nav ul li');
+    for(var i = 0; i < menuItems.length; i++){
+        var element = menuItems[i];
+        if(element.className.indexOf(name) !== -1){
+            element.className += ' active';
+        } else{
+            element.className = element.className.replace(' active', '');
+        }
+    }
+}
 
 function changeActive(name) {
     if (active === name) return;
+    active = name;
+    setActiveMenuItem(name);
     showActivePage(name);
 
 }
